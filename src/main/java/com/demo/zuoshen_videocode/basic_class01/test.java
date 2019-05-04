e8c7d6caa692f3aa417f33f5c231da8836592e11
wangwei <wangwei@risingstarinfo.com>
2019-05-04 13:14:29 +0800

    基本排序

---
+++ src/main/java/com/demo/zuoshen_videocode/basic_class01/test.java
@@ -0,0 +1,24 @@
+package com.demo.zuoshen_videocode.basic_class01;
+
+import ch.qos.logback.classic.pattern.Abbreviator;
+
+/**
+ * @Author: wangwei
+ * @Date: 2019/4/25 17:23
+ * @Version 1.0
+ */
+public class test {
+
+    /**
+     * @Description: 
+     * @Param: null
+     * @return: 
+     * @Date: 2019/4/26
+     * @Author: wangwei
+     */ 
+    public String teet(String ae){
+
+        return "nihao";
+    }
+}
+
---
+++ src/main/java/com/demo/zuoshen_videocode/test.java
@@ -0,0 +1,8 @@
+package com.demo.zuoshen_videocode;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println(Math.random());
+    }
+}
