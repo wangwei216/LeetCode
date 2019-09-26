86ff7f4f9545dd4fa5600090ce0caf1f42522194
wangwei <528175453@qq.com>
2019-09-26 16:07:34 +0800

    删除链表中倒数第K个节点

--- src/main/java/com/wangwei/algorithm/LinkedList/DeleteLastKNode.java
+++ src/main/java/com/wangwei/algorithm/LinkedList/DeleteLastKNode.java
@@ -8,8 +8,45 @@ package com.wangwei.algorithm.LinkedList;
  */
 public class DeleteLastKNode {
 
-    public ListNode deleteLastKNode(ListNode head, int k){
-        return null;
+    /**
+     *  思路：
+     *      1.首先是定义一个快指针一个慢指针
+     *      2.然后快指针先跑传入的K个节点，然后慢指针这时候和快指针同时开始跑，当快指针到头的时候，此时的慢指针就是停留的倒数第K个位置上
+     */
+    public static ListNode deleteLastKNode(ListNode head, int k){
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i=0; i<k; i++){
+            fast = fast.next;
+        }
+        //然后判断要删除的节点是不是第一个节点
+        if (head == null){
+            return head.next;
+        }
+        //然后同时让快指针和慢指针一步一步的跑
+        while (fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        //然后此时的慢指针的位置就是要删除节点的位置
+        slow.next = slow.next.next;
+
+       return head;
     }
 
+    public static void main(String[] args) {
+        // 1->2->3->4->5->6->7->null
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        head1.next.next.next.next = new ListNode(5);
+        head1.next.next.next.next.next = new ListNode(6);
+        ListNode head = deleteLastKNode(head1, 2);
+        while (head != null){
+            System.out.print(head.val+" ");
+            head = head.next;
+        }
+
+    }
 }
