78e359d2fab2cdc1c3cb4c1c2965b411c77382ef
wangwei <wangwei@taptap.com>
2020-10-14 12:30:56 +0800

    完成接雨水

--- src/main/java/com/leetcode/leetcode/editor/cn/[34]在排序数组中查找元素的第一个和最后一个位置.java
+++ src/main/java/com/leetcode/leetcode/editor/cn/[34]在排序数组中查找元素的第一个和最后一个位置.java
@@ -17,16 +17,15 @@ package com.leetcode.leetcode.editor.cn;//给定一个按照升序排列的整�
 // 👍 593 👎 0
 
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         int length = candidates.length;
+        Arrays.sort(candidates);
         Deque<Integer> path = new ArrayDeque<>();
         dfs(res, candidates, target, path, 0);
         return res;
@@ -58,5 +57,40 @@ class Solution {
             path.removeLast();
         }
     }
+
+    /**
+     * 第二种剪枝排序优化
+     *
+     * 有些朋友可能会疑惑什么时候使用 used 数组，什么时候使用 begin 变量。这里为大家简单总结一下：
+     *
+     * 排列问题，讲究顺序（即 [2, 2, 3] 与 [2, 3, 2] 视为不同列表时），需要记录哪些数字已经使用过，此时用 used 数组；
+     * 组合问题，不讲究顺序（即 [2, 2, 3] 与 [2, 3, 2] 视为相同列表时），需要按照某种顺序搜索，此时使用 begin 变量。
+     *
+     * @param res 结果集
+     * @param candidates 参数
+     * @param target 目标值
+     * @param path  选择的路径，好进行回溯
+     * @param begin 搜索的起点
+     */
+    public void dfs2(List<List<Integer>> res, int[] candidates, int target, Deque<Integer> path, int begin) {
+        //如果结果集
+        if (target == 0){
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        for (int i = begin; i < candidates.length; i++) {
+            if (target - candidates[i] < 0){
+                break;
+            }
+            //先进行把这个路径放到path中，
+            path.addLast(candidates[i]);
+            //这里继续进行下一个节点的递归，只有当最后的值为0或者小于0的时候才算结束，就是不会生成新的节点继续往下走了
+            dfs(res, candidates, target - candidates[i] , path, i);
+            //这里其实就是回溯，如果上一步失败了就还能回来
+            path.removeLast();
+        }
+    }
+
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
