6eeffaf317c91dff53510190787a4cc94877bdfa
wangwei <wangwei216@nou.com.cn>
2022-09-02 11:15:00 +0800

    最小覆盖子串

---
+++ src/main/java/com/wangwei/algorithm/algorithmsTop100/Code76.java
@@ -0,0 +1,75 @@
+package com.wangwei.algorithm.algorithmsTop100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Code76 {
+
+    public static void main(String[] args) {
+        Code76 code76 = new Code76();
+        String s = code76.minWindow("AA", "A");
+        System.out.printf(s);
+
+    }
+    /**
+     * 最小覆盖子串
+     * 滑动窗口算法
+     * 1、什么时候应该移动 right 扩大窗口？窗口加入字符时，应该更新哪些数据？
+     * 2、什么时候窗口应该暂停扩大，开始移动 left 缩小窗口？从窗口移出字符时，应该更新哪些数据？
+     * 3、我们要的结果应该在扩大窗口时还是缩小窗口时进行更新？
+     * @param s
+     * @param t
+     * @return
+     */
+    public String minWindow(String s, String t) {
+        // Map 的 key 表示单个字符，value 表示出现的次数
+        Map<Character, Integer> window = new HashMap<>();
+        Map<Character, Integer> target = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            target.put(c, target.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0;
+        int right = 0;
+        int count = 0; // 其中 count 变量表示窗口中满足 target 条件的字符个数，如果 count 和 目标窗口 的大小相同，则说明窗口已满足条件，已经完全覆盖了串 T
+
+        // 用于更新满足的窗口window的长度,如果是len一直是MAX_VALUE，说明没有满足的串
+        int len = Integer.MAX_VALUE;
+        // 用于记录window串的起始位置，则返回 s[start, len]
+        int start = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            // 如果字符在目标Map中出现，那么就需要把这个目标放到滑动窗口Map中，而且还得保证跟目标窗口的该字符出现的次数也得保证一致
+            if (target.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                // 需要更新c 字符出现的次数
+                if (window.get(c).equals(target.get(c))) {
+                    count++;
+                }
+            }
+
+            // 收缩窗口的长度，其实就是符合结果的才移动
+            while (count == target.size()) {
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                char removeChar = s.charAt(left);
+                // 缩小窗口，左指针向左移动，
+                left++;
+
+                // 从窗口移出字符时，应该更新哪些数据？需要判断移除的字符是不是在目标窗口中，如果在的话继续看次数是不是符合目标窗口
+                if (target.containsKey(removeChar)) {
+                    if (target.get(removeChar).equals(window.get(removeChar))) {
+                        count--;
+                    }
+                    window.put(removeChar, window.get(removeChar) -1);
+                }
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+}
